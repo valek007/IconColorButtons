530951564b65693b0f1937b1496f69d8ad4224b9
Valek007 <coronadev.king@gmail.com>
2020-05-11 14:52:51 +0300

    The buttons have icon and description wit ActionColor

--- source/com/app/IconColorButtons.java
+++ source/com/app/IconColorButtons.java
@@ -31,16 +31,34 @@ public class IconColorButtons{
 
 	static class MyPanel extends JPanel{
 
-		private JButton bRed = new JButton("Red");
-		private JButton bYellow = new JButton("Yellow");
-		private JButton bGreen = new JButton("Green");
+		private ActionColor actRed = new ActionColor("Red", new ImageIcon("com/app/resources/red.png"),Color.red);
+		private ActionColor actYellow = new ActionColor("Yellow", new ImageIcon("com/app/resources/yellow.png"),Color.yellow);
+		private ActionColor actGreen = new ActionColor("Green", new ImageIcon("com/app/resources/green.png"),Color.green);
 
 		public MyPanel(){
 
-			add(bRed);
-			add(bYellow);
-			add(bGreen);
+			add(new JButton(actRed));
+			add(new JButton(actYellow));
+			add(new JButton(actGreen));
 		}
+	}
+
+	static class ActionColor extends AbstractAction{
+
+		public ActionColor(String name, Icon icon, Color color_b){
+
+			putValue(Action.NAME, name);
+			putValue(Action.SMALL_ICON, icon);
+			putValue(Action.SHORT_DESCRIPTION, "Changing background color to " + name);
+			putValue("background_color", color_b);
+
+		}
+
+		public void actionPerformed(ActionEvent e){
+
+		}
+
+
 	}
 
 }
